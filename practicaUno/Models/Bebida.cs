8b4b00099a945c878905dfcc4c70c042e2e51fb6
Agustina Luciana Gimenez <agus.luu@gmail.com>
2024-04-23 21:03:15 -0300

    Agregar archivos de proyecto.

---
+++ practicaUno/Models/Bebida.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicaUno.Models
+{
+    internal class Bebida
+    {
+        public string nombre {  get; set; }
+        public int cantidad {  get; set; }
+
+        /*  {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }*/
+
+        public string GetNombre()
+        {
+            return nombre;
+        }
+        public void SetNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public Bebida(string nombre, int cantidad)
+        {
+            this.nombre = nombre;
+            this.cantidad = cantidad;
+        }
+
+        public void tomarse(int cuantoBebio)
+        {
+            this.cantidad -= cuantoBebio;
+        }
+    }
+}
---
+++ practicaUno/Program.cs
@@ -0,0 +1,18 @@
+﻿using practicaUno.Models;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Bebida bebida = new Bebida("coquita", 500);
+        Bebida bebida2 = new Bebida("fanta", 500);
+        //bebida.nombre = "coquita";
+        //bebida.cantidad = 500;
+
+        bebida.tomarse(100);
+        bebida2.tomarse(100);
+        Console.WriteLine("Le quedan "+ bebida.cantidad + " a la bebida: " + bebida.nombre);
+        Console.WriteLine(bebida2.cantidad);
+    }
+}
